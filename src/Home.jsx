31fa27e5111c70e96e41656a605e154eb7b497c3
rivkicobersy <rcobersy@gmail.com>
2022-11-06 22:17:12 +0200

    authorization added

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,14 @@
+import { Signup } from "./Signup";
+import { Login } from "./Login";
+import { LogoutLink } from "./LogoutLink";
+
 export function Home() {
   return (
     <div>
       <h1>Welcome to React!</h1>
+      <Signup />
+      <Login />
+      <LogoutLink />
     </div>
   );
 }
