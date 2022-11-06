3eded8dca26dd2120d35cf1874c00e44aca91205
rivkicobersy <rcobersy@gmail.com>
2022-11-06 22:49:14 +0200

    ingredients index added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import { Signup } from "./Signup";
+import { Login } from "./Login";
+import { LogoutLink } from "./LogoutLink";
 import { Header } from "./Header";
 import { Home } from "./Home";
 import { Footer } from "./Footer";
@@ -6,6 +9,9 @@ function App() {
   return (
     <div>
       <Header />
+      <Signup />
+      <Login />
+      <LogoutLink />
       <Home />
       <Footer />
     </div>
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,14 +1,23 @@
-import { Signup } from "./Signup";
-import { Login } from "./Login";
-import { LogoutLink } from "./LogoutLink";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { IngredientsIndex } from "./IngredientsIndex";
 
 export function Home() {
+  const [ingredients, setIngredients] = useState([]);
+
+  const handleIndexIngredients = () => {
+    console.log("handleIndexIngredients");
+    axios.get("http://localhost:3000/ingredients.json").then((response) => {
+      console.log(response.data);
+      setIngredients(response.data);
+    });
+  };
+
+  useEffect(handleIndexIngredients, []);
+
   return (
     <div>
-      <h1>Welcome to React!</h1>
-      <Signup />
-      <Login />
-      <LogoutLink />
+      <IngredientsIndex ingredients={ingredients} />
     </div>
   );
 }
