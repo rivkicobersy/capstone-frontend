1241f4ecce992a611751d232bcea1e58623d07d3
rivkicobersy <rcobersy@gmail.com>
2022-11-06 22:59:42 +0200

    create new ingredient action added

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 import { IngredientsIndex } from "./IngredientsIndex";
+import { IngredientsNew } from "./IngredientsNew";
 
 export function Home() {
   const [ingredients, setIngredients] = useState([]);
@@ -13,10 +14,19 @@ export function Home() {
     });
   };
 
+  const handleCreateIngredient = (params, successCallback) => {
+    console.log("handleCreateIngredient", params);
+    axios.post("http://localhost:3000/ingredients.json", params).then((response) => {
+      setIngredients([...ingredients, response.data]);
+      successCallback();
+    });
+  };
+
   useEffect(handleIndexIngredients, []);
 
   return (
     <div>
+      <IngredientsNew onCreateIngredient={handleCreateIngredient} />
       <IngredientsIndex ingredients={ingredients} />
     </div>
   );
