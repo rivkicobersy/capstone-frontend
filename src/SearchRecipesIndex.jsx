7be225b118ea89ff41c23f5e2a8fdbace138aa68
rivkicobersy <rcobersy@gmail.com>
2022-11-10 05:23:25 +0200

    add random recipe generator

--- src/SearchRecipesIndex.jsx
+++ src/SearchRecipesIndex.jsx
@@ -1,14 +1,34 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
+import { PantryItemsIndexPage } from "./PantryItemsIndexPage";
 
 export function SearchRecipesIndex() {
   const [searchRecipes, setSearchRecipes] = useState([]);
+  const [pantryItems, setPantryItems] = useState([]);
+  const [searchRecipeName, setSearchRecipeName] = useState("");
+
+  const handleIndexPantryItems = () => {
+    console.log("Going to get all pantry_items...");
+    axios.get("http://localhost:3000/pantry_items.json").then((response) => {
+      console.log(response);
+      setPantryItems(response.data);
+    });
+  };
+
+  useEffect(handleIndexPantryItems, []);
+
   const handleCreateSearchRecipes = (params) => {
     console.log(params.get("recipe"));
     axios.get(`http://localhost:3000/search_recipes.json?q=${params.get("recipe")}`).then((response) => {
       console.log("Created search_recipes", response);
       setSearchRecipes(response.data);
     });
+  };
+
+  const handleClick = () => {
+    console.log(pantryItems);
+    var item = pantryItems[Math.floor(Math.random() * pantryItems.length)];
+    setSearchRecipeName(item.ingredient.name);
   };
 
   const handleSubmit = (event) => {
@@ -23,10 +43,20 @@ export function SearchRecipesIndex() {
         <h1>Search</h1>
         <form onSubmit={handleSubmit}>
           <div>
-            Recipe: <input name="recipe" className="form-control" type="text" />
+            Recipe:{" "}
+            <input
+              value={searchRecipeName}
+              onChange={(event) => setSearchRecipeName(event.target.value)}
+              name="recipe"
+              className="form-control"
+              type="text"
+            />
           </div>
+          <button onClick={handleClick} className="btn btn-primary mt-3 mr-3">
+            Random pantry items
+          </button>
           <button className="btn btn-primary mt-3" type="submit">
-            Searh
+            Search
           </button>
         </form>
         <div className="row">
@@ -49,5 +79,7 @@ export function SearchRecipesIndex() {
   );
 }
 
-// change to new so can fill form
 // fix length of cards
+// TODO add ingredient and pantry items based on recipe
+
+// delete pantry items
