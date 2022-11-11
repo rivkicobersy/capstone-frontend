1e964db994d0dbe2f5d86b807c37f748eadb8f46
rivkicobersy <rcobersy@gmail.com>
2022-11-11 06:25:51 +0200

    delete function added

--- src/FavRecipesIndex.jsx
+++ src/FavRecipesIndex.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 
 export function FavRecipesIndex() {
-  const [FavRecipes, setFavRecipes] = useState([]);
+  const [favRecipes, setFavRecipes] = useState([]);
 
   const handleIndexFavRecipes = () => {
     console.log("Going to get all fav_recipes...");
@@ -12,22 +12,34 @@ export function FavRecipesIndex() {
     });
   };
 
+  const handleDestroyFavRecipe = (favRecipe) => {
+    console.log("handleDestroyFavRecipe", favRecipe);
+    axios.delete(`http://localhost:3000/fav_recipes/${favRecipe.id}.json`).then((response) => {
+      setFavRecipes(favRecipes.filter((p) => p.id !== favRecipe.id));
+    });
+  };
+
   useEffect(handleIndexFavRecipes, []);
 
   return (
     <div className="container">
       <h1>My favourite recipes</h1>
-      {FavRecipes.map((FavRecipe) => (
-        <div key={FavRecipe.id}>
-          <h4>{FavRecipe.label}</h4>
-          <img src={FavRecipe.image} alt="" />
-          <div>
-            <a target="_blank" href={FavRecipe.url}>
-              Link to recipe
-            </a>
+      <div className="row">
+        {favRecipes.map((favRecipe) => (
+          <div className="col-md-3 mb-4" key={favRecipe.id}>
+            <div className="card">
+              <h4>{favRecipe.label}</h4>
+              <img src={favRecipe.image} alt="" />
+              <div>
+                <a target="_blank" href={favRecipe.url}>
+                  Link to recipe
+                </a>
+                <button onClick={() => handleDestroyFavRecipe(favRecipe)}>unfavourite</button>
+              </div>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 }
